1cfb064d575cc99368efc1d178edbbb6d372e2fe
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-15 07:53:37 +0000

    [Fix] アカウント登録、ログイン後のリダイレクト先、bookの投稿が正しくできないのを修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def index
@@ -43,7 +44,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.permit(:title, :body)
   end
 
 end
