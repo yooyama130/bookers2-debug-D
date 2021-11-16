bf41dc1d0a22154edfe6a0db03ca5c62895271da
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-16 07:56:48 +0000

    [Update] indexにのみ、いいね機能作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,21 @@
-class FavoritesController < ApplicationController
+class FavoritesController < ApplicationControllerra
   def create
+    book = Book.find(params[:book_id])
+    favorite = Favorite.new(book_id: book.id, user_id: current_user.id)
+    favorite.save
+    redirect_to books_path
   end
 
   def destroy
+    book = Book.find(params[:book_id])
+    favorite = Favorite.find_by(book_id: book.id, user_id: current_user.id)
+    favorite.destroy
+    redirect_to books_path
   end
+
 end
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
   devise_for :users
   root to: 'homes#top'
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   get 'home/about' => 'homes#about'
 end
