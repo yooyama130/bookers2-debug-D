a78b02c58ae33113a140d2a0205dc3a78e6cd8a7
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-16 02:51:56 +0000

    [Fix] ユーザ更新時のりダイレクト先とエラーメッセージの表示を修正、他人のデータに干渉できないように修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @book = Book.find(params[:id])
@@ -47,5 +48,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    unless @user == current_user
+      redirect_to books_path
+    end
   end
 end
