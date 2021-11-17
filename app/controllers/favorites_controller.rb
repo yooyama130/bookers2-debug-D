e26523fb0033a048d9b88d82067f3aaee83dc25e
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-17 00:32:43 +0000

    [Update] いいね機能を作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,18 @@
-class FavoritesController < ApplicationControllerra
+class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     favorite = Favorite.new(book_id: book.id, user_id: current_user.id)
     favorite.save
-    redirect_to books_path
+    # redirect_to books_path
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = Favorite.find_by(book_id: book.id, user_id: current_user.id)
     favorite.destroy
-    redirect_to books_path
+    # redirect_to books_path
+    redirect_back(fallback_location: root_path)
   end
 
 end
