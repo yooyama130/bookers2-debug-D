641d3c149464c3ff59a396b860d4d5542c1b3b70
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-17 10:32:38 +0000

    [Update] コメントの削除機能、バリデーションを設定

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,22 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      redirect_to book_path(@book)
+    else
+      @comments = BookComment.all
+      @user = @book.user
+       @new_book = Book.find(params[:book_id])
+      render 'books/show'
+    end
   end
 
   def destroy
+    comment = BookComment.find(params[:id])
+    comment.destroy
+    redirect_to book_path(params[:book_id])
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,7 +10,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = Favorite.find_by(book_id: book.id, user_id: current_user.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    
   end
 
 end
