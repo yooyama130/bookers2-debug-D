ea659e6520054874dac1c660b5ad52908f5b8a2b
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-30 00:30:57 +0000

    [Update] コメント機能を非同期通信化※（リロードしないとフォームが再度現れない）

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,19 +4,29 @@ class BookCommentsController < ApplicationController
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
     if @book_comment.save
-      redirect_to book_path(@book)
+      @book = Book.find(params[:book_id])
+      @user = @book.user
+      @new_book = Book.new
+      @comments = BookComment.all
+      @book_comment = BookComment.new
+      # redirect_to book_path(@book)
     else
       @comments = BookComment.all
       @user = @book.user
        @new_book = Book.find(params[:book_id])
-      render 'books/show'
+      # render 'books/show'
     end
   end
 
   def destroy
     comment = BookComment.find(params[:id])
     comment.destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @user = @book.user
+    @new_book = Book.new
+    @comments = BookComment.all
+    @book_comment = BookComment.new
+    # redirect_to book_path(params[:book_id])
   end
 
   private
