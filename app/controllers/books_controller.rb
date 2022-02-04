985c93bee443f6979f10d9abcda550791833a3f8
yooyama130 <yooyama.97.01@gmail.com>
2022-02-04 07:32:02 +0000

    [Finish] 応用課題7d

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -49,7 +49,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body, :evaluation)
   end
 
   def ensure_correct_user
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_11_19_001255) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.float "evaluation"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
